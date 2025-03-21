25f95f3aa7328c84b5c30914aa5f991b3a2ac075
darya <darya@MBP-darya.Home>
2025-03-21 21:42:48 +0000

    initial log

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
 
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT)
+app.listen(PORT, console.log(`Server is running on PORT: ${PORT}`))
