7cbadfc5aeb145719ae63545d0c0400edb52400f
darya <darya@MBP-darya.Home>
2025-03-23 14:59:42 +0000

    add routes for camps

---
+++ routes/camps.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getCamps, getCamp, createCamp, updateCamp, deleteCamp } = require('../controllers/camps');
+
+const router = express.Router();
+router.route('/')
+  .get(getCamps)
+  .post(createCamp);
+
+router.route('/:id')
+  .get(getCamp)
+  .put(updateCamp)
+  .delete(deleteCamp);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const camps = require('./routes/camps');
+const morgan = require("morgan")
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+//Mount routes
+app.use('/api/v1/camps', camps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on PORT: ${PORT}`))
