9758554c8f2a0b4b2f8e7d2822c4a8e0bec55308
darya <darya@MBP-darya.Home>
2025-03-24 21:25:12 +0000

    add mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const camps = require('./routes/camps');
-const morgan = require("morgan")
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const app = express();
 
@@ -18,3 +21,9 @@ app.use('/api/v1/camps', camps);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on PORT: ${PORT}`))
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
+})
