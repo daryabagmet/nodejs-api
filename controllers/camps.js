a5b0c110da06eeb717277e048decee02b3aaa0a0
darya <darya@MBP-darya.Home>
2025-04-02 16:41:37 +0100

    add pagination

--- controllers/camps.js
+++ controllers/camps.js
@@ -33,11 +33,35 @@ exports.getCamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt')
   }
 
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Camp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit)
+
+  const pagination = {};
+
   const campsList = await query;
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   res
     .status(200)
-    .json({ success: true, total: campsList.length, data: campsList })
+    .json({ success: true, total: campsList.length, pagination, data: campsList })
 })
 
 //@desc       Get camp by id
